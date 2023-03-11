c91148e77d58ade3afa12a1310fa0baea2b81656
Javier Coronel <jd.coronel@outlook.com>
2023-03-11 12:48:54 +0100

    Refactor HED edge extraction

--- edges/HED.py
+++ edges/HED.py
@@ -1,19 +1,23 @@
+"""
+Module to extract the edges of an image using HED 
+"""
 from pathlib import Path
 import cv2 as cv
 from skimage import filters
 import numpy as np
+import os
 import skimage as sk
 
 # based on https://github.com/opencv/opencv/blob/master/samples/dnn/edge_detection.py
 class CropLayer(object):
-    def __init__(self):
+    """Modified version of a Crop Layer in OpenCV"""
+
+    def __init__(self, params, blobs):
         self.x_start = 0
         self.x_end = 0
         self.y_start = 0
         self.y_end = 0
 
-    # Our layer receives two inputs. We need to crop the first input blob
-    # to match a shape of the second one (keeping batch size and number of channels)
     def getMemoryShapes(self, inputs):
         input_shape, target_shape = inputs[0], inputs[1]
         batch_size, num_channels = input_shape[0], input_shape[1]
@@ -28,13 +32,37 @@ class CropLayer(object):
         return [inputs[0][:, :, self.y_start : self.y_end, self.x_start : self.x_end]]
 
 
-def run_network(image):
-
-    # Load the pretrained model (source: https://github.com/s9xie/hed)
+def load_network():
+    """Load the pretrained HED model (if files not available, download from source: https://github.com/s9xie/hed)"""
+    #
     script_path = Path(__file__).parent.absolute()
-    net = cv.dnn.readNetFromCaffe(
-        str(script_path / "deploy.prototxt"), str(script_path / "hed_pretrained_bsds.caffemodel")
-    )
+    proto_txt_file = os.path.join(script_path, "deploy.prototxt")
+    assert os.path.isfile(
+        proto_txt_file
+    ), f"{proto_txt_file} does not exist, download from source https://github.com/s9xie/hed"
+
+    pretrained_model_file = os.path.join(script_path, "hed_pretrained_bsds.caffemodel")
+    assert os.path.isfile(
+        pretrained_model_file
+    ), f"{pretrained_model_file} does not exist, download from source https://github.com/s9xie/hed"
+
+    network = cv.dnn.readNetFromCaffe(proto_txt_file, pretrained_model_file)
+    return network
+
+
+def run_hed_network(image):
+    """Runs the HED network for edge extraction on an image
+
+    Parameters
+    ----------
+    image : np.array
+        Image to use as input for the network
+
+    Returns
+    -------
+    output: np.array
+        Output image from the HED model
+    """
     cv.dnn_registerLayer("Crop", CropLayer)
 
     # prepare image as input dataset (mean values from full image dataset)
@@ -46,26 +74,57 @@ def run_network(image):
         swapRB=False,
         crop=False,
     )
-    net.setInput(inp)
-    out = net.forward()
-    cv.dnn_unregisterLayer("Crop")  # get rid of issues when run in a loop
-    out = out[0, 0]
 
-    return out
+    network = load_network()
+    network.setInput(inp)
+    output = network.forward()
+    cv.dnn_unregisterLayer("Crop")  # get rid of issues when run in a loop
+    output = output[0, 0]
+
+    return output
+
+
+def preprocess_image(image):
+    """Preprocess an image for edge extraction using HED
+
+    Parameters
+    ----------
+    image : np.array
+        Image to preprocess
+
+    Returns
+    -------
+    preprocessed_image : np.array
+        Preprocessed image
+    """
+    image = filters.gaussian(image, channel_axis=2)
+    image = image / np.amax(image) * 255
+    image = image.astype(np.uint8)
+    preprocessed_image = cv.resize(image, (image.shape[1], image.shape[0]))
+
+    return preprocessed_image
 
 
 def extract_edges(image):
+    """Extracts the edges of an image using HED. It also applies a threshold and skeletonization to extcract
+    the stronger edges
 
-    image = filters.gaussian(image, sigma=16, truncate=5 / 16, channel_axis=2)
+    Parameters
+    ----------
+    image : np.array
+        Image used to extract te edges
 
-    image = image / np.amax(image) * 255
-    image = image.astype(np.uint8)
-
-    edges = run_network(image)
+    Returns
+    -------
+    edges : np.array
+        Image with the stronger edges
+    """
+    input_image = preprocess_image(image)
+    image_edges = run_hed_network(input_image)
 
     # gray to binary
-    hed_seg = np.ones((edges.shape[0], edges.shape[1]))
-    hed_seg[edges < 0.3] = 0
+    hed_seg = np.ones((image_edges.shape[0], image_edges.shape[1]))
+    hed_seg[image_edges < 0.3] = 0
 
     # skeletonize to get inner lines
     edges = sk.morphology.skeletonize(hed_seg).astype(int)
