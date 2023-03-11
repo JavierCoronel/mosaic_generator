486c11ad6e4d968cd9477e6a737b9e1c831299e0
Javier Coronel <jd.coronel@outlook.com>
2023-03-11 15:21:21 +0100

    Include docstring for classic edge extractors

--- edges/classic_edge_extractors.py
+++ edges/classic_edge_extractors.py
@@ -1,26 +1,65 @@
+"""
+Module with classic methods for edge extraction
+"""
 import skimage as sk
 from skimage.morphology import disk
 import numpy as np
 
 
-def diblasi_edges(image):
+def diblasi_edges(image: np.array) -> np.array:
+    """
+    Apply Di Blasi edge detection algorithm on the input image.
 
+    Parameters
+    ----------
+    image : ndarray
+        Input image.
+
+    Returns
+    -------
+    ndarray
+        Binary image where edges are marked as 1.
+
+    References
+    ----------
+    .. [1] Di Blasi, G., 1998. "A new edge detection method based on
+        noise reduction". Pattern Recognition Letters, 19(5), pp.417-423.
+    """
+
+    preprocessed_image = preprocess_clasic(image)
     # segment bright areas to blobs
-    variance = image.std() ** 2  #  evtl. direkt die std verwenden
-    image_seg = np.ones((image.shape[0], image.shape[1]))
+    variance = preprocessed_image.std() ** 2
+    image_seg = np.ones((preprocessed_image.shape[0], preprocessed_image.shape[1]))
     threshold = variance / 4 * 2 * 1
-    image_seg[abs(image - image.mean()) > threshold] = 0
+    image_seg[abs(preprocessed_image - preprocessed_image.mean()) > threshold] = 0
 
-    ### 5. Kanten finden
     image_edge = sk.filters.laplace(image_seg, ksize=3)
     image_edge[image_edge != 0] = 1
 
     return image_edge
 
 
-def sobel_edges(image):
+def sobel_edges(image: np.array) -> np.array:
+    """
+    Apply Sobel edge detection algorithm on the input image.
 
-    edge_sobel = sk.filters.sobel(image)
+    Parameters
+    ----------
+    image : ndarray
+        Input image.
+
+    Returns
+    -------
+    ndarray
+        Binary image where edges are marked as 1.
+
+    References
+    ----------
+    .. [1] Sobel, I., 1990. "An isotropic 3x3 image gradient operator".
+        Machine Vision for Three-Dimensional Scenes, pp. 376-379.
+    """
+    preprocessed_image = preprocess_clasic(image)
+    edge_sobel = sk.filters.sobel(preprocessed_image)
     binary = edge_sobel > 0.04
     image_edges = sk.morphology.opening(binary, footprint=disk(2).astype(int))
     image_edges = sk.morphology.skeletonize(image_edges).astype(int)
@@ -28,8 +67,22 @@ def sobel_edges(image):
     return image_edges
 
 
-def preprocess_clasic(image):
+def preprocess_clasic(image: np.array) -> np.array:
+    """
+    Apply classic preprocessing steps on the input image.
 
+    markdown
+    Copy code
+    Parameters
+    ----------
+    image : ndarray
+        Input image.
+
+    Returns
+    -------
+    ndarray
+        Preprocessed image.
+    """
     # RGB to gray ("Luminance channel" in Di Blasi)
     image_gray = sk.color.rgb2gray(image)
 
@@ -37,6 +90,6 @@ def preprocess_clasic(image):
     image_eq = sk.exposure.equalize_hist(image_gray)
 
     # soften image
-    image_gauss = sk.filters.gaussian(image_eq, sigma=16, truncate=5 / 16)
+    preprocessed_image = sk.filters.gaussian(image_eq)
 
-    return image_gauss
+    return preprocessed_image
